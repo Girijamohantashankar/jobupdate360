1c453c416c510a2bb0fa1b13680e7b9ae182842b
Girija <jiyocof402@mfyax.com>
2024-07-19 15:27:16 +0530

    job deleted in automatic when expire date is less then the current date

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -1,50 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
+const cron = require('node-cron');
 
+// Endpoint to create a new job
+router.post('/createJob', async (req, res) => {
+  const job = new Job(req.body);
+  try {
+    const saveJob = await job.save();
+    res.status(201).json(saveJob);
 
+    // Schedule job deletion task if not already scheduled
+    scheduleJobDeletionTask();
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-
-// Create a new Job
-router.post('/createJob', async(req,res) =>{
-    const job = new Job(req.body);
-    try{
-        const saveJob = await job.save();
-        res.status(201).json(saveJob);
-    } catch (err) {
-        res.status(400).json({ message: err.message});
-    }
-})
-
-// Get all jobs
-router.get('/allJobs', async (req, res) => {
+function scheduleJobDeletionTask() {
+  cron.schedule('0 0 * * *', async () => {
+    console.log('Cron job started at:', new Date().toLocaleString());
     try {
-      const jobs = await Job.find();
-      res.json(jobs);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+      const currentDate = new Date();
+      const expiredJobs = await Job.find({ expireDate: { $lt: currentDate } });
+
+      console.log('Found expired jobs:', expiredJobs);
+
+      // Delete expired jobs
+      expiredJobs.forEach(async (job) => {
+        await job.remove(); // Assuming Job model has a remove method
+      });
+
+      console.log('Expired job deletion completed.');
+    } catch (error) {
+      console.error('Error running expired job deletion:', error);
     }
   });
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Endpoint to get all jobs
+router.get('/allJobs', async (req, res) => {
+  try {
+    const jobs = await Job.find();
+    res.json(jobs);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
